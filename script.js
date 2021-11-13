e53720cda8e6537057805fe2c658e7c099f99d5f
Jay <jbarbanel531@gmail.com>
2021-11-13 12:30:56 -0800

    added upper case conversion and prompt for character choice

--- script.js
+++ script.js
@@ -21,9 +21,36 @@ var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
+var options;
 
 // Special Characters to choose from :
 
 character = ["!", "#", "$", "&", "*", "$", "@", "+", "-", "=", "<", ">", "\\"];
 
+// Alphabet to choose from :
+
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "W", "x", "y", "z"];
+
+// Uppercase Conversions
+space = [];
+
+var toUpper = function(x) {
+    return x.toUpperCase();
+};
+
+alpha2 = alpha.map(toUpper);
+
+var get = document.querySelector("Generate");
+
+get.addEventListener("click", function() {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
+
+// To Generate :
+
+// This will ask the user yo pick their character and character count :
+function generatePassword() {
+    enter = parseInt(prompt("Please choose your character count.  Must be between 8-128"));
+
+}
